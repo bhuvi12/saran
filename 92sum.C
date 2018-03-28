c3d86765357930f8e92e87403e396afe62c6831b
bhuvi12 <31438639+bhuvi12@users.noreply.github.com>
2018-03-28 16:03:31 +0530

    Create 92sum.C

---
+++ 92sum.C
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int i,n,sum=0,a[10];
+printf("\n enter the number:");
+scanf("%d",&n);
+scanf("%d%d"&a,&b);
+for(i=0;i<n;i++)
+{
+scanf("%d",&a[i]);
+sum=sum+a[i];
+}
+printf("\n  the sum is:",sum);
+getch();
+}
